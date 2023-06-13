9c03ba5e7abd0375f7fcc99d2373e6a26e4ce741
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-06-13 22:32:32 +0900

    feat: add authentication middleware
    Mentions: #1

---
+++ backend/middleware/auth.middleware.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hiroto0222/workout-tracker-app/config"
+	"github.com/hiroto0222/workout-tracker-app/models"
+	"github.com/hiroto0222/workout-tracker-app/utils"
+	"gorm.io/gorm"
+)
+
+func AuthMiddleware(config config.Config, db *gorm.DB) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var token string
+		cookie, err := ctx.Cookie("token")
+
+		authorizationHeader := ctx.Request.Header.Get("Authorization")
+		fields := strings.Fields(authorizationHeader)
+
+		if len(fields) != 0 && fields[0] == "Bearer" {
+			token = fields[1]
+		} else if err == nil {
+			token = cookie
+		}
+
+		if token == "" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"status": "fail", "message": "You are not logged in"})
+			return
+		}
+
+		sub, err := utils.ValidateToken(token, config.JWTSecret)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"status": "fail", "message": err.Error()})
+			return
+		}
+
+		var user models.User
+		result := db.First(&user, "id = ?", fmt.Sprint(sub))
+		if result.Error != nil {
+			ctx.AbortWithStatusJSON(http.StatusForbidden, gin.H{"status": "fail", "message": "the user belonging to this token no logger exists"})
+			return
+		}
+
+		ctx.Set("currentUser", user)
+		ctx.Next()
+	}
+}
