6f84e0365813eb712cf09989f181177d71f1ee7a
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-07-11 16:55:22 +0900

    feat: makes Workout.EndedAt nullable Mentions: #39

--- backend/models/workout.model.go
+++ backend/models/workout.model.go
@@ -6,7 +6,7 @@ type Workout struct {
 	ID        int       `gorm:"type:int AUTO_INCREMENT;primary_key;"`
 	UserID    string    `gorm:"type:varchar;not null"`
 	StartedAt time.Time `gorm:"not null"`
-	EndedAt   time.Time `gorm:"not null"`
+	EndedAt   time.Time `gorm:"null"`
 	UpdatedAt time.Time `gorm:"not null"`
 
 	User User `gorm:"foreignKey:UserID"`
