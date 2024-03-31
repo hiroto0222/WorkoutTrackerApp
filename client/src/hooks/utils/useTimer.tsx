0604887a03995b37fabdefd4ce36f3616e7c1486
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-03-31 22:42:45 +0900

    feat: add timer to workout screen Mentions: #68

---
+++ client/src/hooks/utils/useTimer.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import BackgroundTimer from "react-native-background-timer";
+
+const useTimer = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(true);
+
+  const resetTimer = () => {
+    setSeconds(0);
+    setIsRunning(false);
+  };
+
+  useEffect(() => {
+    let intervalId: number | undefined;
+    if (isRunning) {
+      intervalId = BackgroundTimer.setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds + 1);
+      }, 1000);
+    } else {
+      if (intervalId != undefined) {
+        BackgroundTimer.clearInterval(intervalId);
+        resetTimer();
+      }
+    }
+
+    return () => {
+      if (intervalId != undefined) {
+        BackgroundTimer.clearInterval(intervalId);
+        resetTimer();
+      }
+    };
+  }, [isRunning]);
+
+  return { seconds };
+};
+
+export default useTimer;
---
+++ client/src/utils/index.ts
@@ -0,0 +1,7 @@
+export const formatTime = (seconds: number) => {
+  const mins = Math.floor(seconds / 60);
+  const sec = seconds % 60;
+  return `${mins.toString().padStart(2, "0")}:${sec
+    .toString()
+    .padStart(2, "0")}`;
+};
