405b9c2f19f1aa7186497385ec7b57632595b8a8
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-04-02 18:14:20 +0900

    feat: add exercise controller and service Mentions: #84

---
+++ backend/services/exercise.service.go
@@ -0,0 +1,28 @@
+package services
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hiroto0222/workout-tracker-app/models"
+	"gorm.io/gorm"
+)
+
+type ExerciseService interface {
+	GetExercises(ctx *gin.Context)
+}
+
+type ExerciseServiceImpl struct {
+	db *gorm.DB
+}
+
+// GetExercises retrieves all exercises
+func (s *ExerciseServiceImpl) GetExercises(ctx *gin.Context) {
+	var exercises []models.Exercise
+	res := s.db.Find(&exercises)
+	if res.Error != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to retrieve exercises"})
+	}
+
+	ctx.JSON(http.StatusOK, exercises)
+}
