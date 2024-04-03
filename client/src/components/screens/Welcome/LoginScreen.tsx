4813de64a20a573e01da24b88f2ffa0591e743bb
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-04-04 03:10:05 +0900

    fix: login screen not showing text Mentions: #107

--- client/src/components/screens/Welcome/LoginScreen.tsx
+++ client/src/components/screens/Welcome/LoginScreen.tsx
@@ -1,9 +1,9 @@
+import globalStyles from "components/styles";
 import * as WebBrowser from "expo-web-browser";
 import useGoogleLogin from "hooks/auth/useGoogleLogin";
 import useLogin from "hooks/auth/useLogin";
 import useSignUp from "hooks/auth/useSignUp";
 import React, { useState } from "react";
-import { View } from "react-native";
 import { Button, Div, Image, Input, Text } from "react-native-magnus";
 
 WebBrowser.maybeCompleteAuthSession();
@@ -16,9 +16,15 @@ const LoginScreen = () => {
   const { login } = useLogin();
 
   return (
-    <View>
+    <>
       <Div flex={1} mt="2xl">
-        <Text mt="3xl" mx="xl" w="70%" fontWeight="bold" fontSize="5xl">
+        <Text
+          mt="3xl"
+          mx="xl"
+          w="70%"
+          fontSize="5xl"
+          style={globalStyles.textMedium}
+        >
           Get Started
         </Text>
         <Input
@@ -114,7 +120,7 @@ const LoginScreen = () => {
           </Button>
         </Div>
       </Div>
-    </View>
+    </>
   );
 };
 
