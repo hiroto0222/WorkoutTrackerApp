52ddcd346af3358032e9d5d1ac1c24fbcb557bbe
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-04-27 19:27:30 +0900

    feat: add local firebase auth client during dev

--- backend/config/default.go
+++ backend/config/default.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -18,10 +17,6 @@ type Config struct {
 
 	// firebase config
 	FirebaseServiceAcccountKey string `mapstructure:"FIREBASE_SERVICE_ACCOUNT_KEY"`
-
-	// oauth config
-	GoogleOAuthClientID     string `mapstructure:"GOOGLE_OAUTH_CLIENT_ID"`
-	GoogleOAuthClientSecret string `mapstructure:"GOOGLE_OAUTH_CLIENT_SECRET"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
@@ -32,14 +27,6 @@ func LoadConfig(path string) (config Config, err error) {
 		config.Port = os.Getenv("PORT")
 		config.DBSource = os.Getenv("DATABASE_URL")
 		config.FirebaseServiceAcccountKey = os.Getenv("FIREBASE_SERVICE_ACCOUNT_KEY")
-		config.GoogleOAuthClientID = os.Getenv("GOOGLE_OAUTH_CLIENT_ID")
-		config.GoogleOAuthClientSecret = os.Getenv("GOOGLE_OAUTH_CLIENT_SECRET")
-		log.Printf("Origin: %v", config.Origin)
-		log.Printf("Port: %v", config.Port)
-		log.Printf("DBSource: %v", config.DBSource)
-		log.Printf("FirebaseServiceAccountKey: %v", config.FirebaseServiceAcccountKey)
-		log.Printf("GoogleOAuthClientID: %v", config.GoogleOAuthClientID)
-		log.Printf("GoogleOAuthClientSecret: %v", config.GoogleOAuthClientSecret)
 		return
 	}
 
--- backend/config/firebase.go
+++ backend/config/firebase.go
@@ -5,17 +5,29 @@ import (
 	"log"
 
 	b64 "encoding/base64"
+	"encoding/json"
 
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
 	"google.golang.org/api/option"
 )
 
+type serviceAccountKey struct {
+	ProjectID string `json:"project_id"`
+}
+
 func InitAuth(config Config) (*auth.Client, error) {
 	// decode base64 json
 	bytes, err := b64.StdEncoding.DecodeString(config.FirebaseServiceAcccountKey)
 	if err != nil {
 		log.Fatalf("Failed to decode base64 json Firebase service-account-key: %v", err)
+	}
+
+	// parse to json
+	var serviceAccountKey serviceAccountKey
+	err = json.Unmarshal(bytes, &serviceAccountKey)
+	if err != nil {
+		log.Fatalf("Failed to parse service account key JSON: %v", err)
 	}
 
 	// create a Firebase app instance
@@ -30,6 +42,8 @@ func InitAuth(config Config) (*auth.Client, error) {
 	if err != nil {
 		log.Fatalf("Failed to create Firebase auth client: %v", err)
 	}
+
+	log.Println("Successfully initialized firebase auth client:", serviceAccountKey.ProjectID)
 
 	return authClient, nil
 }
