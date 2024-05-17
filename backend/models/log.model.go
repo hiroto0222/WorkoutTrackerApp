0f0f8e59672e96c9e8084beab9bf7a03253b66ae
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-05-17 13:45:37 +0900

    feat: change logs.weight, logs.time data type from int to numeric(5, 2)

--- backend/models/log.model.go
+++ backend/models/log.model.go
@@ -1,12 +1,12 @@
 package models
 
 type Log struct {
-	ID                int `gorm:"type:int AUTO_INCREMENT;primary_key;"`
-	WorkoutExerciseID int `gorm:"type:int;not null"`
-	SetNumber         int `gorm:"not null"`
-	Weight            int `gorm:"type:int"`
-	Reps              int `gorm:"type:int"`
-	Time              int `gorm:"type:int"`
+	ID                int     `gorm:"type:int AUTO_INCREMENT;primary_key;"`
+	WorkoutExerciseID int     `gorm:"type:int;not null"`
+	SetNumber         int     `gorm:"type:int;not null"`
+	Weight            float64 `gorm:"type:numeric(5,2)"`
+	Reps              int     `gorm:"type:int"`
+	Time              float64 `gorm:"type:numeric(5,2)"`
 
 	WorkoutExercise WorkoutExercise `gorm:"foreignKey:WorkoutExerciseID"`
 }
